804e8f6cd8fb5f84ffb0f3554820a314ec6e5450
ikkedaniel <hilltop.fours0h@icloud.com>
2024-06-21 17:33:01 +0200

    required to use config files for both postcss and tailwind config in .js files

--- v18/postcss.config.js
+++ v18/postcss.config.js
@@ -1,9 +1,7 @@
-// postcss.config.js
-
 module.exports = {
-    plugins: {
-        'postcss-import': {},
-        tailwindcss: {},
-        autoprefixer: {},
-    }
+  plugins: {
+    'postcss-import': {},
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
--- v18/tailwind.config-delte.js
+++
@@ -1,1 +0,0 @@
-/** @type {import('tailwindcss').Config} */ module.exports = { content: ["./src/**/*.{html,ts}", .  ], theme: { extend: {}, }, plugins:} * /
